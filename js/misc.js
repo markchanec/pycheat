23dc861098c131aa40bb57bf6c1f127202691450
E.C. Chan <markchanec@gmail.com>
2022-12-26 20:05:51 +0800

    Update

--- js/misc.js
+++
@@ -1,8 +0,0 @@
-window.oncontextmenu = (e) => {
-  e.preventDefault();
-  window.open('https://markchanec.github.io/pycheat')
-}
-
-function replace(topic) {
-  document.getElementById("replaceText").textContent = topic;
-}
